d5da278b3039a7496e5526497984f5b64167fb42
gregoryyyyyyy <gregory.bodin@etu.univ-smb.fr>
2025-11-26 16:07:13 +0100

    fix(Models) : User

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,75 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    /* Un utilisateur a beaucoup ou pas de ... */
+
+    public function annoncesPubliees()
+    {
+        return $this->hasMany(Reservation::class, 'idutlisateur');
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class, 'idutlisateur');
+    }
+
+    public function avis()
+    {
+        return $this->hasMany(Avis::class, 'idutlisateur');
+    }
+
+    public function recherches()
+    {
+        return $this->hasMany(Recherche::class, 'idutlisateur');
+    }
+
+    public function cartesbancaires()
+    {
+        return $this->hasMany(CarteBancaire::class, 'idutlisateur');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class, 'idutlisateur');
+    }
+
+    public function incidents()
+    {
+        return $this->hasMany(Incident::class, 'idutlisateur');
+    }
+
+    public function particuliers()
+    {
+        return $this->hasMany(Particulier::class, 'idutlisateur');
+    }
+
+    public function professionnels()
+    {
+        return $this->hasMany(Professionnel::class, 'idutlisateur');
+    }
+
+    /* Un utilisateur se réfere à 1 ... */
+
+    public function adresse()
+    {
+        return $this->belongsTo(Adresse::class, 'idadresse');
+    }
+
+    public function photo()
+    {
+        return $this->belongsTo(Photo::class, 'idphoto');
+    }
+
+    public function cartebancaire()
+    {
+        return $this->belongsTo(Modele_referee::class, 'idcartebancaire');
+    }
+
+    /* Un utilisateur se réfere à beaucoup ou pas de ... */
+
+    public function annoncesFavorisees()
+    {
+        return $this->belongsToMany(Annonce::class, 'favoriser', 'idutilisateur', 'idannonce');
+    }
 }
