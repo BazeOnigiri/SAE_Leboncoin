fb5bd590dc589c20a9927899ebee2b96e95bbc5c
gregoryyyyyyy <gregory.bodin@etu.univ-smb.fr>
2025-11-26 15:10:54 +0100

    fix(Models) : TypeHebergement

--- app/Models/TypeHebergement.php
+++ app/Models/TypeHebergement.php
@@ -8,6 +8,26 @@ class TypeHebergement extends Model
 {
     protected $table = 'typehebergement';
     protected $primaryKey = 'idtypehebergement';
-
     public $timestamps = false;
+
+    /* Un type d'hebergement a beaucoup ou pas de ... */
+
+    public function annonces()
+    {
+        return $this->hasMany(Modele_referee::class, 'idtypehebergement');
+    }
+
+    /* Un type d'hebergement se réfere à 1 ... */
+
+    public function categorie()
+    {
+        return $this->belongsTo(Categorie::class, 'idcategorie');
+    }
+
+    /* Un type d'hebergement se réfere à beaucoup ou pas de ... */
+
+    public function recherches()
+    {
+        return $this->belongsToMany(Recherche::class, 'cibler', 'idtypehebergement', 'idrecherche');
+    }
 }
