6e41d6d78765e040f2940a3b0d4a966e880a45e2
BazeOnigiri <timkiehl@icloud.com>
2025-11-27 18:09:24 +0100

    docs(rechercher annonce): ajouter un composant livewire

---
+++ app/Livewire/AnnonceList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AnnonceList extends Component
+{
+    public function render()
+    {
+        return view('livewire.annonce-list');
+    }
+}
