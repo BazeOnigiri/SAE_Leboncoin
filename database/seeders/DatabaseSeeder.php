3941aac16394c5717f099f2fa4aea6cae836bef2
August7337 <augustin.tarit@gmail.com>
2025-12-02 22:03:05 +0100

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(InsertSeeder::class);
+        $this->call(InsertSeeder::class);
         $user = User::create([
             'nomutilisateur' => 'TestNom',
             'prenomutilisateur' => 'TestPrenom',
