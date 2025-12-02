6f81e4243ded8febbd68718bcc991b168af89ab9
BazeOnigiri <timkiehl@icloud.com>
2025-12-02 19:52:28 +0100

    feat(routes) : ajouter les routes pour les modifications du compte utilisateur

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@ use App\Http\Controllers\AnnonceController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ConnexionController;
 use App\Http\Controllers\CNIController;
+use App\Http\Controllers\UserAccountController;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -30,4 +31,9 @@ Route::middleware([
 
         Route::get('/cni', [CNIController::class, 'index'])->name('cni.index');
         Route::post('/cni', [CNIController::class, 'store'])->name('cni.store');
+        Route::get('/compte/profil/modifier', [UserAccountController::class, 'edit'])->name('user.edit');
+        Route::get('/compte/mes-annonces', [UserAccountController::class, 'annonces'])->name('user.annonces');
+        Route::get('/compte/profil-espaces', [UserAccountController::class, 'spaces'])->name('user.spaces');
+        Route::get('/compte/connexion-securite', [UserAccountController::class, 'security'])->name('user.security');
+        Route::get('/compte/parametres', [UserAccountController::class, 'settings'])->name('user.settings');
     });
