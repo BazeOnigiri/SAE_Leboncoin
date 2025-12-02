363c295d45e95f9bc8b5a1a743314ee8dbc15c08
August7337 <augustin.tarit@gmail.com>
2025-12-02 22:51:38 +0100

    feat(alerts): add alert component with success, error, and info messages fix(CNIController): redirect to dashboard with info message if CNI is validated docs(readme): update usage instructions and add examples

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-## Usage
+# Usage
+# Docker
 ```
 docker build -t laravel-app . 
 ```
@@ -12,7 +13,7 @@ git config --global user.email "you@example.com"
 ```
 git config --global user.name "Your Name"
 ```
-# DB conf
+## DB conf
 Setup tables : 
 ```
 php artisan migrate
@@ -22,6 +23,26 @@ Add inserts
 php artisan db:seed
 ```
 
-# User from debit seed
+## User from debit seed
 email: ```test@example.com```
 password: ```password```
+
+# Doc
+## Allerts
+
+need a view with app layout
+
+success exemple :
+```php
+return back()->with('success', 'Votre CNI est déjà vérifiée.');
+```
+
+error exemple :
+```php
+return back()->withErrors(['error' => 'Une erreur est survenue.']);
+```
+
+info exemple :
+```php
+return back()->with('info', 'Votre CNI est déjà vérifiée.');
+```
--- app/Http/Controllers/CNIController.php
+++ app/Http/Controllers/CNIController.php
@@ -7,10 +7,16 @@ use Illuminate\Http\Request;
 class CNIController extends Controller
 {
     public function index() {
+        if (auth()->user()->isCNIValidate()) {
+            return redirect()->route('dashboard')->with('info', 'Votre CNI est déjà vérifiée.');
+        }
         return view("cni.index");
     }
 
     public function store(Request $request) {
+        if (auth()->user()->isCNIValidate()) {
+            return redirect()->route('dashboard')->with('info', 'Votre CNI est déjà vérifiée.');
+        }
         $validated = $request->validate([
             'recto' => 'required|file|image|mimes:jpeg,png,jpg|max:5120',
             'verso' => 'required|file|image|mimes:jpeg,png,jpg|max:5120',
@@ -24,7 +30,7 @@ class CNIController extends Controller
         $rectoPath = $request->file('recto')->store('cni/' . $idutilisateur . '/recto', 'local');
         $versoPath = $request->file('verso')->store('cni/' . $idutilisateur . '/verso', 'local');
 
-        return redirect()->back()->with([
+        return redirect()->route('dashboard')->with([
             'success' => 'CNI traité avec succès.',
         ]);
     }
