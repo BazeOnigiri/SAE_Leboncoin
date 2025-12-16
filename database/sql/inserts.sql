1f4c8072a68b6e4cde9b2d2ba272dee233e27678
gregoryyyyyyy <gregory.bodin@etu.univ-smb.fr>
2025-12-16 10:36:36 +0100

    fix(inserts.sql) : suppression de doublon

--- database/sql/inserts.sql
+++ database/sql/inserts.sql
@@ -3055,7 +3055,6 @@ INSERT INTO reservation(idannonce,iddatedebutreservation,iddatefinreservation,id
 (25,44689,44693,24,'Garcon','Rachel','0652211742'),
 (25,44709,44713,25,'Rademaker','Quentin','0772562261'),
 (28,46020,46024,25,'Rademaker','Quentin','0772562261'),
-(28,46020,46024,25,'Rademaker','Quentin','0772562261'),
 (3,46035,46038,40,'Berger','Claire','0678877669');
 
 /*==============================================================*/
