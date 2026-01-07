f5be16730859baa60dec283906b606c813bec53c
BazeOnigiri <timkiehl@icloud.com>
2026-01-07 21:26:17 +0100

    fix : enlever d'autre truc inutile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -123,26 +123,6 @@
             </div>
 
         </div>
-        <div class="w-full overflow-x-auto no-scrollbar py-2 mt-4">
-            <div class="flex flex-wrap gap-3 font-sans">
-                <button
-                    class="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium text-slate-700 bg-blue-50 border border-slate-700 rounded-xl hover:bg-blue-100 transition-colors">
-                    <span>Locations saisonnières</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-4 h-4">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                    </svg>
-                </button>
-                <button
-                    class="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium text-slate-700 bg-blue-50 border border-slate-700 rounded-xl hover:bg-blue-100 transition-colors">
-                    <span>Tri : Pertinence</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-4 h-4">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                    </svg>
-                </button>
-            </div>
-        </div>
 
         <div class="mt-8">
             <livewire:annonce-list />
