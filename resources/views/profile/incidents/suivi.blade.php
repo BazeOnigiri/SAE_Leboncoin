7bd391a79b441c933d7b221d2ad097927da32562
gregoryyyyyyy <gregory.bodin@etu.univ-smb.fr>
2026-01-13 08:21:14 +0100

    fix : changement message refus remboursement

--- resources/views/profile/incidents/suivi.blade.php
+++ resources/views/profile/incidents/suivi.blade.php
@@ -80,7 +80,7 @@
                                         <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"/></svg>
                                     </div>
                                     <h4 class="text-xl font-bold text-red-700 mb-2">Remboursement Refusé</h4>
-                                    <p class="text-gray-600 mb-6">Après analyse des éléments, nos services n'ont pas pu valider votre demande de remboursement pour ce motif.</p>
+                                    <p class="text-gray-600 mb-6">Après analyse des éléments, nos services n'ont pas pu valider votre demande de remboursement.</p>
                                 @endif
 
                                 {{-- ACTIONS POSSIBLES (Uniquement à l'étape 4) --}}
